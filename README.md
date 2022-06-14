ce3155a4dee70a829640faf97cd825de3f5686fe
Fabiano Filho <fabianobnfilho09@gmail.com>
2022-06-14 16:07:27 -0300

    adicionando o readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Login e Cadastro de usuarios utilizando o framework Django e a lib django-allauth
--- signInUp/settings.py
+++ signInUp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',	
     'allauth.socialaccount',
     # Local apps
-
+    
 ]
 
 MIDDLEWARE = [
