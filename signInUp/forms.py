cfbc3b9d6bd486c1632ddb764c7b9cc4c17b958b
Fabiano Filho <fabianobnfilho09@gmail.com>
2022-06-19 21:22:25 -0300

    feat: adicionando error de login e cadastro no html

--- signInUp/forms.py
+++ signInUp/forms.py
@@ -2,6 +2,8 @@ from allauth.account.forms import LoginForm, SignupForm
 from django import forms
 
 class MyLoginForm(LoginForm):
+    error_css_class = 'error'
+    required_css_class = 'required'
 
     def __init__(self, *args, **kwargs):
         super(MyLoginForm, self).__init__(*args, **kwargs)
