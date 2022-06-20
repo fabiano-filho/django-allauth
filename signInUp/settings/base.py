6764dee178487b627de9bee528f25c4e9446315c
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-06-20 10:26:21 -0300

    fix: testando config para deploy

--- signInUp/settings/base.py
+++ signInUp/settings/base.py
@@ -127,10 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -160,7 +163,3 @@ LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_SIGNUP_REDIRECT_URL = '/accounts/login'
 
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = False
-
-# WhiteNoise
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
